7e876e1c305ff0147f7a643d62198b09400b1f71
Sanat Deshpande <sanatdeshpandejhu@gmail.com>
2017-10-29 16:38:34 -0400

    functional canvas for one drawer

--- app.js
+++ app.js
@@ -7,19 +7,44 @@ function init() {
 
 function initializeCanvas(canvas) {
     canvas.element = initializeStyle(canvas.element);
-
+    canvas.isDrawing = false;
     canvas.ctx = canvas.element.getContext('2d');
 
     canvas.element.onmousedown = function(e) {
-        console.log(e.x, e.y);
+        if (canvas.isDrawing) {
+            return;
+        }
+        canvas.x = e.x;
+        canvas.y = e.y;
+        canvas.isDrawing = true;
     }
 
+    canvas.element.onmouseup = function(e) {
+        canvas.isDrawing = false;
+    }
+
+    canvas.element.onmousemove = function(e) {
+        if (!canvas.isDrawing) {
+            return;
+        }
+        canvas.ctx.beginPath();
+        canvas.ctx.moveTo(canvas.x, canvas.y);
+        canvas.ctx.lineTo(e.x, e.y);
+        canvas.ctx.stroke();
+        canvas.x = e.x;
+        canvas.y = e.y;
+    }
+
+    canvas.element.onmouseleave = function(e) {
+        canvas.isDrawing = false;
+    }
     return canvas;
 }
 
+
 function initializeStyle(canvas) {
-    canvas.style.width = '80%';
-    canvas.style.height = '80%';
+    canvas.setAttribute('width', document.body.clientWidth * .8 + 'px');
+    canvas.setAttribute('height', document.body.clientHeight * .8 + 'px');
     canvas.style.borderStyle = 'solid';
     canvas.style.borderWidth = '3px';
     canvas.style.borderColor = 'black';
